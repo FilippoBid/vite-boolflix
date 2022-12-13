0260144f2fc0f7a9e39957db5042e1a599861c78
FilippoBid <FilippoPerrone1997@gmail.com>
2022-12-13 12:01:38 +0100

    carico axios in store

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,25 @@
+<script>
+import{store} from "../store";
+
+export default {
+
+  data() {
+    return {
+        store,
+     
+    }
+  }
+}
+</script>
+
+
+<template>
+  <button class="btn btn-danger rounded-5 ">cerca</button>
+  <input type="text" placeholder="cerca film">
+</template>
+
+
+
+<style>
+
+</style>
